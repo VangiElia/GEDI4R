7d2f8b3535aa0abaed624c6b5add33a1620a1266
Elia Vangi <97439095+VangiElia@users.noreply.github.com>
2022-03-30 12:39:43 +0200

    Add files via upload

---
+++ R/GEDI4R.R
@@ -0,0 +1,15 @@
+#' GEDI4R: A package for NASA's Global Ecosystem Dynamics Investigation (GEDI)
+#' Level 4A Data Visualizing and Processing.
+#'
+#' The GEDI4R package provides functions for i) downloading, ii) reading, iii)
+#' clipping, iv)visualizing and v) exporting GEDI data. The output of many
+#' function is compatible with functions in the
+#' \href{https://github.com/carlos-alberto-silva/rGEDI}{rGEDI} package. See the
+#' package readme for an overview of the package functionality.
+#' @note See the
+#'   \href{https://daac.ornl.gov/GEDI/guides/GEDI_L4A_AGB_Density.html}{user
+#'   guide} for more documentation on GEDI L4 data.
+#' @docType package
+#' @name GEDI4R
+#' @author Elia Vangi
+NULL
